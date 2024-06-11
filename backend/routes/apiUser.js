7ec77f520da364bd8cce7aa4f2c3f09627235e55
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-06-11 15:10:07 -0600

    chore: Update /hello route to return a 200 status code

--- backend/routes/apiUser.js
+++ backend/routes/apiUser.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Ejemplo de ruta GET
 router.get("/hello", (req, res) => {
-  res.json({ message: "Hello, world!" });
+  res.status(200).json({ message: "Hello, world!" });
 });
 
 // Agrega más rutas aquí...

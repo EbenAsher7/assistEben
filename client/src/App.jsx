bc9217bef20fc50cea214341718ac0ea9ae9f68b
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-06-13 16:58:44 -0600

    chore: Add @radix-ui/react-dropdown-menu for DARK MODE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Atom } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./components/ThemeToggle";
 
 const App = () => {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
+    <div className="flex w-full h-screen items-center justify-center bg-neutral-100 dark:bg-neutral-900">
       <Button>
         <Atom className="mr-2 h-4 w-4" /> Pueba de botón ShadCN e Iconos con tailwind
       </Button>
+      <ThemeToggle />
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </>
 );

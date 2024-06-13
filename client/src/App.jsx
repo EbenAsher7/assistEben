99e4d57b0d74207d884f59ed6311f55e16dc778b
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-06-13 16:45:27 -0600

    Clean project and add ShadCN w Tailwind for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Atom } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="flex w-full h-screen items-center justify-center">
+      <Button>
+        <Atom className="mr-2 h-4 w-4" /> Pueba de botón ShadCN e Iconos con tailwind
+      </Button>
+    </div>
+  );
+};
 
-export default App
+export default App;

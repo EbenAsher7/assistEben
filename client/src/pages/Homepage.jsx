e4c754343502fbb93ea9f5aaf18097f2945e75ca
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-06-14 10:51:48 -0600

    feat: Improve attendance page layout and add student name prompt on homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,5 +1,10 @@
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      Homepage
+      <p>Aqui irá la pantalla donde se pregunta al usuario su nombre antes de cambiar a tipo de asistencia</p>
+    </div>
+  );
 };
 
 export default Homepage;

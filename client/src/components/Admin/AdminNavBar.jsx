5a727ed3b33642c6fa6e84721afc9e4a92d726c2
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-08-18 08:56:25 -0600

    feat: Add "Lista de Estudiantes" and "Estadísticas" tabs to AdminNavBar
    The AdminNavBar component is updated to include two new tabs: "Lista de Estudiantes" and "Estadísticas". These tabs allow the user to navigate to the corresponding sections in the admin panel. This enhancement provides additional functionality and improves the user experience by providing easy access to important information and statistics.

--- client/src/components/Admin/AdminNavBar.jsx
+++ client/src/components/Admin/AdminNavBar.jsx
@@ -4,17 +4,23 @@ import AdminTutorNavbar from "./AdminTutorNavbar";
 import AdminModuleNavbar from "./AdminModuleNavbar";
 
 const AdminNavBar = () => {
-  const [selectedTab, setSelectedTab] = useTabs(["tutores", "modulos"]);
+  const [selectedTab, setSelectedTab] = useTabs(["tutores", "modulos", "listStudens", "analytics"]);
 
   return (
     <>
-      <h1 className="text-center text-xl font-extrabold mt-24 mb-2">PANEL ADMINSTRATIVO</h1>
+      <h1 className="text-center text-xl font-extrabold mt-24 mb-2">PANEL ADMINISTRATIVO</h1>
       <nav className="flex border-b border-gray-300 sm:w-[80%] w-full m-auto justify-center">
         <TabSelector isActive={selectedTab === "tutores"} onClick={() => setSelectedTab("tutores")}>
           Tutores
         </TabSelector>
         <TabSelector isActive={selectedTab === "modulos"} onClick={() => setSelectedTab("modulos")}>
           Módulos
+        </TabSelector>
+        <TabSelector isActive={selectedTab === "listStudens"} onClick={() => setSelectedTab("listStudens")}>
+          Lista de Estudiantes
+        </TabSelector>
+        <TabSelector isActive={selectedTab === "analytics"} onClick={() => setSelectedTab("analytics")}>
+          Estadísticas
         </TabSelector>
       </nav>
       <div className="py-4 sm:w-[80%] w-full m-auto">
@@ -23,6 +29,12 @@ const AdminNavBar = () => {
         </TabPanel>
         <TabPanel hidden={selectedTab !== "modulos"}>
           <AdminModuleNavbar />
+        </TabPanel>
+        <TabPanel hidden={selectedTab !== "listStudens"}>
+          <h1>Lista de Estudiantes</h1>
+        </TabPanel>
+        <TabPanel hidden={selectedTab !== "analytics"}>
+          <h1>Estadísticas</h1>
         </TabPanel>
       </div>
     </>

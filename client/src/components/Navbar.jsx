3b9a343afdb8ad1311c9bbe3f0a05b2663900a2b
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-08-21 11:24:56 -0600

    fix: Update handleLogout function in Navbar component
    The handleLogout function in the Navbar component is updated to navigate to the "/login" route instead of the "/" route when the user logs out. This ensures that the user is redirected to the login page after logging out. Additionally, the setIsLogin, setUser, and setIsNavbarOpen states are reset to their initial values. This fix improves the functionality and user experience of the Navbar component.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = memo(() => {
   }, []);
 
   const handleLogout = useCallback(() => {
-    navigate("/");
+    navigate("/login");
     setIsLogin(false);
     setUser({});
     setIsNavbarOpen(false);

ea27b9992c08a55715cdc14985b45133b8467fb4
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-08-21 12:05:14 -0600

    feat: Update ListaCursos component to display courses in a responsive grid
    The ListaCursos component in the Admin/SubModules directory is updated to display the courses in a responsive grid layout. The grid now has one column on small screens and two columns on larger screens. This improves the visual presentation and usability of the component.

--- client/src/components/Admin/SubModules/ListaCursos.jsx
+++ client/src/components/Admin/SubModules/ListaCursos.jsx
@@ -181,7 +181,7 @@ const ListaCursos = () => {
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 mt-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2">
         {cursos &&
           cursos.map((curso) => (
             <div

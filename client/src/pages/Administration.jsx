9763a94f3f1e54f773cae811cd2332b47e01bb80
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-08-22 14:24:01 -0600

    Fix page administration access, remove 1 seg to prevent show navbar administration

--- client/src/pages/Administration.jsx
+++ client/src/pages/Administration.jsx
@@ -8,52 +8,27 @@ const Administration = () => {
   const navigate = useNavigate();
   const [accessDenied, setAccessDenied] = useState(false);
   const [countdown, setCountdown] = useState(5);
-  const [timeoutId, setTimeoutId] = useState(null);
 
   useEffect(() => {
     if (user) {
-      const timeoutId = setTimeout(() => {
-        if (!user.tipo) {
-          setAccessDenied(true);
-          const intervalId = setInterval(() => {
-            setCountdown((prevCountdown) => prevCountdown - 1);
-          }, 1000);
-
-          setTimeout(() => {
-            clearInterval(intervalId);
-            navigate("/");
-          }, 5000);
-        }
-      }, 1000); // Espera 1 segundo
-
-      setTimeoutId(timeoutId);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
+      if (!user.tipo || user.tipo !== "Administrador") {
+        setAccessDenied(true);
+        const intervalId = setInterval(() => {
+          setCountdown((prevCountdown) => {
+            if (prevCountdown === 1) {
+              clearInterval(intervalId);
+              navigate("/");
+            }
+            return prevCountdown - 1;
+          });
+        }, 1000);
+      }
     }
   }, [user, navigate]);
 
-  useEffect(() => {
-    if (user && user.tipo) {
-      clearTimeout(timeoutId);
-      if (user.tipo !== "Administrador") {
-        setAccessDenied(true);
-        const intervalId = setInterval(() => {
-          setCountdown((prevCountdown) => prevCountdown - 1);
-        }, 1000);
-
-        setTimeout(() => {
-          clearInterval(intervalId);
-          navigate("/");
-        }, 5000);
-      }
-    }
-  }, [user, navigate, timeoutId]);
-
   if (accessDenied) {
     return (
-      <div className="w-full h-screen -mt-[60px] justify-center items-center flex flex-col">
+      <div className="w-full h-screen -mt-[60px] justify-center items-center flex flex-col text-center px-8">
         <h1 className="text-3xl font-extrabold text-red-500">No tienes acceso a este módulo.</h1>
         <p className="text-lg italic opacity-60">Serás redirigido a la página de inicio en {countdown} segundos...</p>
       </div>

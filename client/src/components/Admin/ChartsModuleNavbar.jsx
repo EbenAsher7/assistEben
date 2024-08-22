b68486729e25faaa534a77c551d7c90faea76107
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-08-22 08:37:51 -0600

    ```text refactor: Update ChartsModuleNavbar component to include "Asistencia" tab
    The ChartsModuleNavbar component in the client/src/components/Admin/ChartsModuleNavbar.jsx file is updated to include an "Asistencia" tab. This enhancement improves the navigation options and provides a dedicated tab for managing attendance.

--- client/src/components/Admin/ChartsModuleNavbar.jsx
+++ client/src/components/Admin/ChartsModuleNavbar.jsx
@@ -2,12 +2,15 @@ import { TabPanel, useTabs } from "react-headless-tabs";
 import { TabSelector } from "./TabsSelector";
 
 const ChartsModuleNavbar = () => {
-  const [selectedTab, setSelectedTab] = useTabs(["alumnos", "modulos"]);
+  const [selectedTab, setSelectedTab] = useTabs(["asistencia", "alumnos", "modulos"]);
 
   return (
     <>
       <nav className="flex border-b border-gray-300 w-full m-auto justify-center">
-        <TabSelector isActive={selectedTab === "alumnos"} onClick={() => setSelectedTab("alumnos")} color="lime">
+        <TabSelector isActive={selectedTab === "asistencia"} onClick={() => setSelectedTab("asistencia")} color="lime">
+          Asistencia
+        </TabSelector>
+        <TabSelector isActive={selectedTab === "alumnos"} onClick={() => setSelectedTab("alumnos")} color="violet">
           Alumnos
         </TabSelector>
         <TabSelector isActive={selectedTab === "modulos"} onClick={() => setSelectedTab("modulos")} color="blue">
@@ -15,6 +18,9 @@ const ChartsModuleNavbar = () => {
         </TabSelector>
       </nav>
       <div className="p-4 w-full m-auto">
+        <TabPanel render="lazy" hidden={selectedTab !== "asistencia"}>
+          Asistencia
+        </TabPanel>
         <TabPanel render="lazy" hidden={selectedTab !== "alumnos"}>
           Alumnos
         </TabPanel>

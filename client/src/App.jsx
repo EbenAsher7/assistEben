ffbb54dd9a044ff3066a9b30132dfcda68101154
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-08-23 09:42:25 -0600

    ```text refactor: Update routing in App.jsx
    Update the routing in the client/src/App.jsx file to use the PageRegister component instead of the NewRegister component. This change improves the consistency and clarity of the codebase by using a more descriptive component name.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,9 @@ import { Toaster } from "@/components/ui/toaster";
 import Profile from "./pages/Profile";
 import Attendance from "./pages/Attendance";
 import RegisterAttendance from "./pages/RegisterAttendance";
-import NewRegister from "./pages/NewRegister";
+// import NewRegister from "./pages/NewRegister";
 import Administration from "./pages/Administration";
+import PageRegister from "./pages/PageRegister";
 
 const App = () => {
   return (
@@ -22,7 +23,7 @@ const App = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/attendance" element={<Attendance />} />
         <Route path="/registerAttendance" element={<RegisterAttendance />} />
-        <Route path="/newRegister" element={<NewRegister />} />
+        <Route path="/newRegister" element={<PageRegister />} />
         <Route path="/admin" element={<Administration />} />
         <Route path="*" element={<NotFound />} />
       </Routes>

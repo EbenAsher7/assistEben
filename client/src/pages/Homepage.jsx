cf65620367d700c673792794d789f48a21dab0e6
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-11-19 11:39:54 -0600

    Update Homepage layout: remove unnecessary inversion from logos and adjust footer text

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -20,7 +20,7 @@ const Homepage = () => {
       <div className="flex-grow flex justify-center items-center flex-col sm:flex-row background-pattern">
         <div className="flex flex-col gap-3 justify-center max-w-[400px] items-center sm:mr-24">
           <img src="/cropped-favicon.png" alt="logo Ebenezer" className="hidden sm:inline-flex size-32 sm:size-64 invert dark:invert-0" />
-          <img src="/LOGODOCTRINA.webp" alt="logo Ebenezer" className="inline-flex sm:hidden h-16 mb-4 invert-0 dark:invert" />
+          <img src="/LOGODOCTRINA.webp" alt="logo Ebenezer" className="inline-flex sm:hidden h-16 mb-4" />
         </div>
         <div className="flex flex-col gap-3 sm:-mt-24">
           <div className="sm:min-w-[300px] sm:max-w-[700px] w-full">
@@ -35,13 +35,13 @@ const Homepage = () => {
       </div>
 
       <footer className="bg-neutral-800 text-white text-center py-4 flex justify-center gap-2 items-center fixed bottom-0 w-full">
-        <p>Sistema creado por Cristopher Paiz&nbsp;&nbsp;|&nbsp;&nbsp;Contáctame&nbsp;</p>
+        <p>Sistema hecho por Cristopher Paiz&nbsp;&nbsp;|&nbsp;&nbsp;Contáctame&nbsp;</p>
         <div className="space-x-4 flex flex-row items-center">
           <a href="https://wa.me/+50238639275" target="_blank" rel="noopener noreferrer">
             <img src="/whatsapp.png" alt="WhatsApp" className="h-6 w-6 invert-0 dark:invert" />
           </a>
           <a href="https://www.facebook.com/choper.paiz" target="_blank" rel="noopener noreferrer">
-            <img src="/facebook.png" alt="Facebook" className="h-6 w-6 invert-0 dark:invert" />
+            <img src="/facebook.png" alt="Facebook" className="h-6 w-6" />
           </a>
         </div>
       </footer>

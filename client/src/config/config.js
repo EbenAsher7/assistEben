8ea07e0a9b7852d2222afcf386358b637fc62ac9
CristopherPaiz <cpaizl1@miumg.edu.gt>
2024-11-19 11:37:00 -0600

    Update URL_BASE to use production environment URL

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,3 +1,3 @@
 //Export url
-export const URL_BASE = "http://localhost:3000";
-// export const URL_BASE = "https://grubby-deena-cristopherprojects-bbac0c66.koyeb.app";
+// export const URL_BASE = "http://localhost:3000";
+export const URL_BASE = "https://grubby-deena-cristopherprojects-bbac0c66.koyeb.app";
